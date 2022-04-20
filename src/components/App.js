da07a45da2912245d738e90b533c71f342265201
+ <+>
2022-04-20 16:34:33 -0400

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
+// import ListingCard from "./ListingCard";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
- 
+  const [search, setSearch] = useState("")
+
+  function handleSearchChange(newSearch){
+    setSearch(newSearch)
+  }
+
 
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header handleSearchChange={handleSearchChange} />
+      <ListingsContainer search={search} />
+      
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,25 +1,32 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
-  const [listings, setListings] = useState()
-  console.log(listings)
+function ListingsContainer({search}) {
+  const [listings, setListings] = useState([]);
+  // console.log(listings.length)
 
   useEffect(() => {
     fetch(" http://localhost:6001/listings")
-    .then((response) => response.json())
-    .then((listings) => setListings(listings));
+      .then((response) => response.json())
+      .then((data) => setListings(data));
   }, []);
+
+  function handleDeleteListing(id){
+    const updatedListings = listings.filter(listing => listing.id !== id
+    );
+    setListings(updatedListings)
+  }
+
+  const filteredListings = listings.filter(listing =>{
+    return listing.description.toLowerCase().includes(search.toLowerCase())
+  });
+
 
   return (
     <main>
       <ul className="cards">
-        {listings?.map((listing) => (
-          < ListingCard
-          key={listing.id}
-          listing={listing}
-          image={listing.image}
-          />
+        {filteredListings.map((listing) => (
+          <ListingCard key={listing.id} listing={listing} handleDeleteListing={handleDeleteListing} />
         ))}
       </ul>
     </main>
